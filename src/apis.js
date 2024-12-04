b318c35beceebb83e6b8a1943321aab4e20882f3
Soumitra-Mandal19 <mandal.soumitra@outlook.com>
2024-12-05 05:01:45 +0530

    Update fetch logic

--- src/apis.js
+++ src/apis.js
@@ -5,9 +5,7 @@ const countries_api_url = "https://crio-location-selector.onrender.com/countries
 export const fetchCountries = async () => {
     try {
         const response = await fetch(countries_api_url);
-        let data = await response.json();
-        data = data.map((item) => item.trim()).sort();
-        data = [...new Set(data)].sort();
+        const data = await response.json();
         return data;
     } catch (error) {
         console.log(error.message)
@@ -19,9 +17,7 @@ export const fetchCountries = async () => {
 export const fetchStates = async (countryName) => {
     try {
         const response = await fetch(`https://crio-location-selector.onrender.com/country=${countryName}/states`);
-        let data = await response.json();
-        data = data.map((item) => item.trim()).sort();
-        data = [...new Set(data)].sort();
+        const data = await response.json();
         return data;
     } catch (error) {
         console.log(error.message)
@@ -32,9 +28,7 @@ export const fetchStates = async (countryName) => {
 export const fetchCities = async (countryName, stateName) => {
     try {
         const response = await fetch(`https://crio-location-selector.onrender.com/country=${countryName}/state=${stateName}/cities`);
-        let data = await response.json();
-        data = data.map((item) => item.trim()).sort();
-        data = [...new Set(data)].sort();
+        const data = await response.json();
         return data;
     } catch (error) {
         console.log(error.message)
